a6f1fd3f957f38b4b073b956ae8c16b0ca991c19
Jacqueline Ta <z5361055@ad.unsw.edu.au>
2025-06-02 22:53:58 +1000

    Add new path for Bases : base/baseid/tableid

--- src/app/base/[baseId]/[tableId]/page.tsx
+++ src/app/base/[baseId]/[tableId]/page.tsx
@@ -3,26 +3,30 @@
 import React from 'react'
 import { useParams } from 'next/navigation'
 import { api } from '~/trpc/react';
+import TopBar from '../../../_components/BasePage/TopBar';
+import DataTable from '../../../_components/BasePage/DataTable';
 
 const BasePage = () => {
-  const { id } = useParams()
+  const { baseId, tableId } = useParams()
   const { data: base, isLoading, isError } = api.base.getBaseById.useQuery(
-    { id: id as string },
-    { enabled: !!id }
+    { id: baseId as string },
+    { enabled: !!baseId }
   )
 
   if (isLoading) return <div>Loading...</div>
   if (isError || !base) return <div>Error loading base data.</div>
 
   return (
-    <div className="p-4">
+    <div>
+      <TopBar/>
       { base && <>
-        <h1 className="text-xl font-bold">Welcome to {base.name}!</h1>
+        {/* <h1 className="text-xl font-bold">Welcome to {base.name}!</h1>
         <p>Base ID: {base.id}</p>
-        <p>Owner: {base.user.name}</p>
+        <p>Owner: {base.user.name}</p> */}
+        
+        <DataTable tableId={tableId as string}/>
       </>
       }
-     
     </div>
   )
 }
---
+++ src/app/base/[baseId]/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import React from 'react'
+import { useParams } from 'next/navigation'
+import { api } from '~/trpc/react';
+import TopBar from '../../_components/BasePage/TopBar';
+import DataTable from '../../_components/BasePage/DataTable';
+
+const BasePage = () => {
+  const { baseId } = useParams()
+  const { data: base, isLoading, isError } = api.base.getBaseById.useQuery(
+    { id: baseId as string },
+    { enabled: !!baseId }
+  )
+
+  if (isLoading) return <div>Loading...</div>
+  if (isError || !base) return <div>Error loading base data.</div>
+
+  return (
+    <div>
+      <TopBar/>
+      { base && <>
+        <h1 className="text-xl font-bold">NO TABLES for {base.name}!</h1>
+        <p>Base ID: {base.id}</p>
+        <p>Owner: {base.user.name}</p>
+      </>
+      }
+    </div>
+  )
+}
+
+export default BasePage
